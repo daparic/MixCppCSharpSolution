54a972d7c291911bd968b2dc0ce3c6c7ccd66fa4
truthadjuster <truthadjuster@gmail.com>
2023-03-30 17:41:34 +0800

    init

---
+++ projects/ATMExample/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int addnum(int a, int b) {
+    return a + b;
+}
+
+bool decide(bool a, bool b, bool c) {
+    if (a && b && c) {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    std::cout << "*** Parasoft C/C++test Demo ***\n";
+    return 0;
+}
